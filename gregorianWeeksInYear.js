a203e6de7a67a243de1b750959623e5590115a5a
tizah <davidzagi93@gmail.com>
2020-01-30 06:07:53 +0100

    code to generate the gregorian calendar in a year

---
+++ gregorianWeeksInYear.js
@@ -0,0 +1,52 @@
+const moment = require("moment");
+
+let year = 2020;
+let month = 0;
+let startDate = "";
+let endDate = "";
+
+const monthsInYear = [];
+
+while (month < 12) {
+  let isLeapYear = moment([year]).isLeapYear();
+
+  if (isLeapYear) {
+    if (month > 0) {
+      startDate = moment([year, month])
+        .startOf("month")
+        .isoWeekday(1)
+        .add(7, "days");
+    } else {
+      startDate = moment([year, month])
+        .startOf("month")
+        .isoWeekday(1);
+    }
+
+    endDate = moment([year, month])
+      .endOf("month")
+      .isoWeekday(1)
+      .add(5, "days")
+      .subtract(1, "days");
+  } else {
+    startDate = moment([year, month])
+      .startOf("month")
+      .isoWeekday(1);
+
+    endDate = moment([year, month])
+      .endOf("month")
+      .isoWeekday(1)
+      .add(4, "days");
+  }
+  monthsInYear.push({
+    start: moment(startDate).format("YYYY-MM-DD"),
+    end: moment(endDate).format("YYYY-MM-DD"),
+    name: moment(startDate)
+      .add(4, "days")
+      .format("MMMM")
+  });
+
+  month++;
+}
+
+console.log(monthsInYear);
+return monthsInYear;
